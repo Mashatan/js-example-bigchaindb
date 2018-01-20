6d60ff58f11c4d675e4fc643856ac164a50d7e79
Ali Mashatan <ali.mashatan@gmail.com>
2018-01-20 18:30:57 +0330

    minor correction

--- transactions.js
+++ transactions.js
@@ -2,40 +2,35 @@ const driver = require('bigchaindb-driver')
 const hexToBinary = require('hex-to-binary')
 var _bs = require('bs58');
 
-// BigchainDB server instance or IPDB (e.g. https://test.ipdb.io/api/v1/)
 const API_PATH = 'https://test.bigchaindb.com/api/v1/'
 
-// Create a new keypair.
 const alice = new driver.Ed25519Keypair()
 
-var publicInner ="CRPj1bAnxrsSeBtRo9QKHkJzxFRD62fDchpLecEnNVEq"
-var privateInner ="6WVmvaDmPFQQwVwc39o7eYoymKQLGM8ZTD7FadJsZ6E3"
-var publicHex = (_bs.decode(publicInner))
-var privateHex = (_bs.decode(privateInner))
+var publicInner ="ErrnYeJSZP6dybKSNMZCEpnzYN8L1cCfk22TPcPaSfVw"
+var privateInner ="551SwNxgSEWyT2b65HvUyDj4SK54Y7NXpCNkKiUJN2tS"
+var publicBuf = (_bs.decode(publicInner))
+var privateBuf = (_bs.decode(privateInner))
+
+var privateData = privateBuf.toString('binary');
+var privateHex = new Buffer(privateData, 'ascii').toString('hex');
+
+var publicData = publicBuf.toString('binary');
+var publicHex = new Buffer(publicData, 'ascii').toString('hex');
+
 console.log(publicHex)
 console.log(privateHex)
-// Construct a transaction payload
 const tx = driver.Transaction.makeCreateTransaction(
-    // Define the asset to store, in this example it is the current temperature
-    // (in Celsius) for the city of Berlin.
-    { city: 'Berlin, DE', temperature: 22, datetime: new Date().toString() },
-
-    // Metadata contains information about the transaction itself
-    // (can be `null` if not needed)
-    { what: 'My first BigchainDB transaction' },
-
-    // A transaction needs an output
+    { AssetKey: "AssetValue" },
+    { MetaDataKey: "MetaDataValue" },
     [ driver.Transaction.makeOutput(
             driver.Transaction.makeEd25519Condition(publicInner))
     ],
     publicInner
 )
 
-// Sign the transaction with private keys
 const txSigned = driver.Transaction.signTransaction(tx, privateInner)
 console.log(txSigned)
-// Send the transaction off to BigchainDB
-/*
+
 const conn = new driver.Connection(API_PATH, { 
     app_id: 'b1d63ff3',
     app_key: '29913c6deb7ee2bd0709d6af3b382b44'
@@ -44,4 +39,3 @@ conn.postTransaction(txSigned)
     .then(() => conn.pollStatusAndFetchTransaction(txSigned.id))
     .then(retrievedTx => console.log('Transaction', retrievedTx.id, 'successfully posted.'))
 
-*/
